4b1b7d8a51fd2628e9b83fc499e0289dc8c24c8a
ahmadmm01 <94947950+ahmadmm01@users.noreply.github.com>
2022-11-15 00:12:22 +0700

    first commit

---
+++ src/main/java/pws/a/formKTM140/FormKtm140Application.java
@@ -0,0 +1,13 @@
+package pws.a.formKTM140;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormKtm140Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormKtm140Application.class, args);
+	}
+
+}
---
+++ src/test/java/pws/a/formKTM140/FormKtm140ApplicationTests.java
@@ -0,0 +1,13 @@
+package pws.a.formKTM140;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormKtm140ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
