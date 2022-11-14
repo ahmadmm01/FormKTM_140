2c5d5f11a6d7f762a36a48c569357dd488e4fc3d
ahmadmm01 <94947950+ahmadmm01@users.noreply.github.com>
2022-11-15 00:19:53 +0700

    add java controller sources

---
+++ src/main/java/pws/a/formKTM140/Controller.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pws.a.formKTM140;
+
+/**
+ *
+ * @author MADD
+ */
+public class Controller 
+{
+    
+    
+}
